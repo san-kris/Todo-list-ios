741aa47fd4a9cde528e48d445a561c63c2658e9f
Santosh Krishnamurthy <san.b350@gmail.com>
2023-01-24 13:20:20 -0600

    Update data storage to use CoreData SQLite Entity. Add Search Bar to search ToDo list and display result

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
-class ToDoListViewController: UITableViewController {
+class ToDoListViewController: UITableViewController{
 
-    var toDoList: ToDoList?
+    var toDoList = [ToDoItem]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     // Not using userDefaults.
     // let userDefaults = UserDefaults()
@@ -22,6 +24,9 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Print the path to documents folder
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!)
+                
         // Reading UserDefaults file
         /*
         if let userList = userDefaults.object(forKey: "ToDoList") as? [String]{
@@ -41,7 +46,7 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // return the number of rows in a given section
         // print("\(toDoList.count) rows in section \(section)")
-        return toDoList?.items.count ?? 0
+        return toDoList.count
     }
     
     
@@ -55,25 +60,28 @@ class ToDoListViewController: UITableViewController {
         var contentConfig = cell.defaultContentConfiguration()
         
         // Update details in configuration
-        contentConfig.text = toDoList?.items[indexPath.row].title
+        contentConfig.text = toDoList[indexPath.row].title
         contentConfig.textProperties.color = UIColor.red
         contentConfig.textProperties.font = UIFont.systemFont(ofSize: 20)
         
         // Apply updated configuration back to cell
         cell.contentConfiguration = contentConfig
         
-        cell.accessoryType = (toDoList?.items[indexPath.row].done ?? false) ? UITableViewCell.AccessoryType.checkmark : UITableViewCell.AccessoryType.none
+        cell.accessoryType = (toDoList[indexPath.row].done) ? UITableViewCell.AccessoryType.checkmark : UITableViewCell.AccessoryType.none
 
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("user selected \(toDoList?.items[indexPath.row].title ?? "") with done: \(String(describing: toDoList?.items[indexPath.row].done))")
+        print("user selected \(toDoList[indexPath.row].title ?? "") with done: \(toDoList[indexPath.row].done)")
         tableView.deselectRow(at: indexPath, animated: true)
         
-        toDoList?.items[indexPath.row].done = !(toDoList?.items[indexPath.row].done ?? false)
+        // you can change the value of a property by using setvalue method
+        toDoList[indexPath.row].setValue(!(toDoList[indexPath.row].done), forKey: "done")
+        // you can directly access the property of the entiy and update its value
+        // toDoList[indexPath.row].done = !(toDoList[indexPath.row].done)
         
-        tableView.cellForRow(at: indexPath)?.accessoryType = toDoList?.items[indexPath.row].done == true ? UITableViewCell.AccessoryType.checkmark : UITableViewCell.AccessoryType.none
+        tableView.cellForRow(at: indexPath)?.accessoryType = toDoList[indexPath.row].done ? UITableViewCell.AccessoryType.checkmark : UITableViewCell.AccessoryType.none
         
         saveItem()
         // tableView.reloadRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
@@ -92,8 +100,18 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add", style: UIAlertAction.Style.default) { action in
             // We are able to access the textField because it is a class ref variable and not local to the closure
             if let newToDoItem = textField.text, newToDoItem != ""{
+
+                // create new item to be stored in CoreData Entity
+                // The Entity is same as table and is represented by a class.
+                // The class is subclassed from NSManagedObject
+                // each row in Entity / Table is an instance of the class
+                let newItem = ToDoItem(context: self.context)
+                newItem.done = false
+                newItem.title = newToDoItem
+                self.toDoList.append(newItem)
+                
                 // Add new Item to array
-                self.toDoList?.add(title: newToDoItem)
+                // self.toDoList?.add(title: newToDoItem)
                 
                 // Save the array in the user defaults plist against a key
                 // self.userDefaults.set(self.toDoList?.items, forKey: "ToDoList")
@@ -114,7 +132,39 @@ class ToDoListViewController: UITableViewController {
         // Present alert VC
         present(alert, animated: true)
     }
+
+    func saveItem() -> Void {
+        do{
+            try context.save()
+        } catch {
+            print("Error saving to CoreData: \(error)")
+        }
+        
+        
+        // reload table view
+        self.tableView.reloadData()
+    }
+
+    func loadData() -> Void {
+        do{
+            // create a  fetch request from Entity Type Method
+            let request: NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest()
+            toDoList = try context.fetch(request)
+        } catch {
+            print("Error Fetching Item array from SQLITE -  \(error)")
+        }
+        
+    }
     
+    func deleteData(atRow indexPath: IndexPath) -> Void{
+        print("Deleting row \(indexPath.row) with Title: \(String(describing: toDoList[indexPath.row].title) )")
+        // The data in context / staging area will be updated to mark the row to be deleted
+        context.delete(toDoList[indexPath.row])
+        // We need to commit the changes to Context by saving the changes
+        saveItem()
+    }
+    
+    /* used for saving data using encoder
     func saveItem() -> Void {
         // saving new data in a plist in Documents folder
         let encoder = PropertyListEncoder()
@@ -129,7 +179,9 @@ class ToDoListViewController: UITableViewController {
         // reload table view
         self.tableView.reloadData()
     }
+     */
     
+    /* used to load data from file using decoder
     func loadData() -> Void {
         do{
             guard let data = try? Data(contentsOf: toDoItemsDocuentPath!) else {
@@ -145,5 +197,44 @@ class ToDoListViewController: UITableViewController {
         }
         
     }
+     */
+    
 }
 
+extension ToDoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // create a empty request. similar to select *
+        let request: NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest()
+        
+        // Add where condition / filters
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = predicate
+        
+        // add sorting condition for result
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        
+        // fetch the result
+        do{
+            toDoList = try context.fetch(request)
+        } catch {
+            print("Error Fetching Item array from SQLITE -  \(error)")
+        }
+        
+        tableView.reloadData()
+
+        print(searchBar.text!)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadData()
+            tableView.reloadData()
+            // Any time we are updating UI we should do it on the main thread / Queue
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
